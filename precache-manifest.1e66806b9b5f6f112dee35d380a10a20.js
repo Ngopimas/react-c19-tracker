259fa7c32aed2ed507efe04a189f76902d6abb43
Romain Coupey <schaft115@hotmail.com>
2020-08-24 23:47:37 +0200

    Updates

---
+++ precache-manifest.1e66806b9b5f6f112dee35d380a10a20.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "879978ba9597b288d2bba362d4a25c53",
+    "url": "/react-c19-tracker/index.html"
+  },
+  {
+    "revision": "d1cd18d55ae3ac407ac6",
+    "url": "/react-c19-tracker/static/css/main.c95acbe6.chunk.css"
+  },
+  {
+    "revision": "8cd4b9d5b0433f7a4bf6",
+    "url": "/react-c19-tracker/static/js/2.6779b2fe.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/react-c19-tracker/static/js/2.6779b2fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1cd18d55ae3ac407ac6",
+    "url": "/react-c19-tracker/static/js/main.a4650054.chunk.js"
+  },
+  {
+    "revision": "85fb9a51f55e0b8cac19",
+    "url": "/react-c19-tracker/static/js/runtime-main.744aa98e.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/react-c19-tracker/static/media/image.d7265326.png"
+  }
+]);
